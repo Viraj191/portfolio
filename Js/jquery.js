6021efba8e32aadb9565b63b3f0c461a711c228b
viraj191 <virajnikam191@gmail.com>
2022-01-28 13:11:17 +0530

    added Js File

---
+++ Js/jquery.js
@@ -0,0 +1,8 @@
+
+        $("#para").fadeIn(2000);
+        $(".name").animate({ fontSize: "50px" }, 2000);
+        // $("a").animate({fontSize:"18px"},2000);
+        $("a").fadeIn(2000)
+        $(".hello").css("color", "Green");
+        $("img").fadeIn(2000);
+        $(".hello").animate({ fontSize: "20px" }, 3000)
